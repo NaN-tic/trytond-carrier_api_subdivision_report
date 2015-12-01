89287bcf9a21b401073ed52d9b7354b2e003d669
resteve <resteve@zikzakmedia.com>
2015-12-01 11:03:51 +0100

    Tests

--- tests/test_carrier_api_subdivision_report.py
+++ tests/test_carrier_api_subdivision_report.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part carrier_api_subdivision_report module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the carrier_api_subdivision_report module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierApiSubdivisionReportTestCase(unittest.TestCase):
-    'Test Carrier API Subdivision Report module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_api_subdivision_report')
-
-    def test0005views(self):
-        'Test views'
-        test_view('carrier_api_subdivision_report')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class CarrierApiSubdivisionReportTestCase(ModuleTestCase):
+    'Test Carrier Api Subdivision Report module'
+    module = 'carrier_api_subdivision_report'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            CarrierApiSubdivisionReportTestCase))
+        CarrierApiSubdivisionReportTestCase))
     return suite
